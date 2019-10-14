1eb07967a8ad24d5665e05a9bba2dc84ed54364f
diego-ferrand <diego.ferrand@abstracta.com.uy>
2019-10-13 21:37:31 -0300

    Update setup file.

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from distutils.core import setup
 setup(
-  name = 'Oculow',         # How you named your package folder (MyLib)
-  packages = ['Oculow'],   # Chose the same as "name"
-  version = '0.1.1',      # Start with a small number and increase it with every change you make
-  license='Apache 2',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Visual testing framework',   # Give a short description about your library
-  author = 'Diego Ferrand',                   # Type in your name
-  author_email = 'potosin@live.com',      # Type in your E-Mail
-  url = 'https://github.com/oculow/oculow-python-sdk.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['VISUAL', 'TESTING', 'AI', 'EASY'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'Oculow',
+  packages = ['Oculow'],
+  version = '0.1.2',
+  license='Apache 2',
+  description = 'Visual testing framework',   # Give a short description about library
+  author = 'Diego Ferrand',
+  author_email = 'potosin@live.com',
+  url = 'https://github.com/oculow/oculow-python-sdk.git',
+  download_url = 'https://github.com/oculow/oculow-python-sdk/archive/v0.1.2.zip',
+  keywords = ['VISUAL', 'TESTING', 'AI', 'EASY'],
+  install_requires=[
           'validators',
           'beautifulsoup4',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache 2',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: Apache 2',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
