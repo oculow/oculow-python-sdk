6813a8549bc06955ef16790c7a45b6271d29ca89
diego-ferrand <diego.ferrand@abstracta.com.uy>
2019-10-13 21:04:07 -0300

    Rename package to match name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'oculow-python-sdk',         # How you named your package folder (MyLib)
-  packages = ['Oculow'],   # Chose the same as "name"
+  packages = ['oculow-python-sdk'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='Apache 2',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Visual testing framework',   # Give a short description about your library
