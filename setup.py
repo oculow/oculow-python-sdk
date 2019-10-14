f3279cea427844755a78785d7addccee163ff745
diego-ferrand <diego.ferrand@abstracta.com.uy>
2019-10-13 21:37:54 -0300

    Reformat setup

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 from distutils.core import setup
+
 setup(
-  name = 'Oculow',
-  packages = ['Oculow'],
-  version = '0.1.2',
-  license='Apache 2',
-  description = 'Visual testing framework',   # Give a short description about library
-  author = 'Diego Ferrand',
-  author_email = 'potosin@live.com',
-  url = 'https://github.com/oculow/oculow-python-sdk.git',
-  download_url = 'https://github.com/oculow/oculow-python-sdk/archive/v0.1.2.zip',
-  keywords = ['VISUAL', 'TESTING', 'AI', 'EASY'],
-  install_requires=[
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache 2',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name='Oculow',
+    packages=['Oculow'],
+    version='0.1.2',
+    license='Apache 2',
+    description='Visual testing framework',  # Give a short description about library
+    author='Diego Ferrand',
+    author_email='potosin@live.com',
+    url='https://github.com/oculow/oculow-python-sdk.git',
+    download_url='https://github.com/oculow/oculow-python-sdk/archive/v0.1.2.zip',
+    keywords=['VISUAL', 'TESTING', 'AI', 'EASY'],
+    install_requires=[
+        'validators',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
